6d37edd79d73a8645ef380ea46e0fa5cbbb62cd8
Ryan Coleman <ryan@puppetlabs.com>
2012-03-20 14:24:10 -0700

    Confine the Fact to the Darwin Kernel.

--- lib/facter/macosx_mobileconfig_profiles.rb
+++ lib/facter/macosx_mobileconfig_profiles.rb
@@ -1,5 +1,6 @@
 Facter.add(:macosx_mobileconfig_profiles) do
   setcode do
+    confine :kernel => "Darwin"
     profiles = %x{/usr/bin/profiles -P | /usr/bin/grep _computerlevel | awk '{ print $4 }'}.split("\n")
     profiles.join(',')
   end
