68af3e074497f9aad9e2339b471716ca7c44bc1e
Ryan Coleman <ryan@puppetlabs.com>
2012-05-02 22:52:21 -0400

    Refactor: Use Native Type & Fact
    This commit completely scraps the Google python script/service way
    of managing mobileconfig profiles.
    
    Instead, the profile_manager type is created which simply works
    with the profiles command to create, destroy and check on profiles.
    A stupid simple fact is created to show all included profiles.
    
    The type could be improved to do parsing in Ruby and to support user
    versus computer or domain profiles and the fact could be improved
    to use system_profiler and retrieve either structured data (facter
    release willing) or return a new fact for each installed profile.

---
+++ lib/puppet/type/profile_manager.rb
@@ -0,0 +1,23 @@
+Puppet::Type.newtype(:profile_manager) do
+
+  @doc = <<-EOT
+    Manage Apple Configuration Profiles
+    http://help.apple.com/profilemanager/mac/10.7/#apd88330954-6FA0-4568-A88E-7F6828E763A7
+
+    Example Usage:
+      profile_manager { 'com.puppetlabs.foo':
+        ensure   => present,
+        profile  => '/path/to/profile.mobileconfig',
+      }
+
+    The namevar for this type is the identifier for the profile.
+    Profile = path to the profile on the client system.
+
+
+  EOT
+
+  ensurable
+
+  newparam(:name, :namevar => true)
+  newparam(:profile)
+end
