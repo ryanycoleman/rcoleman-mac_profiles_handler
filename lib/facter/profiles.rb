c73fa42a172b43aca60f304255e0c69b7886498e
Samuel Keeley <sam.keeley@gmail.com>
2016-08-01 08:55:32 -0500

    full path to awk

--- lib/facter/profiles.rb
+++ lib/facter/profiles.rb
@@ -2,7 +2,7 @@ Facter.add(:profiles) do
   confine :kernel => "Darwin"
   confine :macosx_productversion_major => %w{10.7 10.8 10.9 10.10 10.11 10.12}
   setcode do
-    profiles = %x{/usr/bin/profiles -P | /usr/bin/grep profileIdentifier | awk '{ print $4 }'}.split("\n")
+    profiles = %x{/usr/bin/profiles -P | /usr/bin/grep profileIdentifier | /usr/bin/awk '{ print $4 }'}.split("\n")
     profiles.join(',')
   end
 end
