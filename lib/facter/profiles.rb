be33be46233fc818a30573efea8646f34e97a5f3
Samuel Keeley <sam.keeley@gmail.com>
2016-08-01 09:09:50 -0500

    we did not really need to grep and awk

--- lib/facter/profiles.rb
+++ lib/facter/profiles.rb
@@ -2,7 +2,16 @@ Facter.add(:profiles) do
   confine :kernel => "Darwin"
   confine :macosx_productversion_major => %w{10.7 10.8 10.9 10.10 10.11 10.12}
   setcode do
-    profiles = %x{/usr/bin/profiles -P | /usr/bin/grep profileIdentifier | /usr/bin/awk '{ print $4 }'}.split("\n")
-    profiles.join(',')
+	profiles = []
+
+	output = %x{/usr/bin/profiles -P}
+
+	output.each_line do |line|
+		if line.include? 'profileIdentifier'
+			profiles << line.split(' ')[3]
+		end
+	end
+
+	profiles.join(',')
   end
 end
