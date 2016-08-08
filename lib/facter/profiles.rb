d448baef2c55ddf6fcc0e4b6ef75a4c3040f7040
Samuel Keeley <sam.keeley@gmail.com>
2016-08-08 15:05:11 -0500

    Use system profiler and get more information

--- lib/facter/profiles.rb
+++ lib/facter/profiles.rb
@@ -1,15 +1,24 @@
+require 'puppet/util/plist'
+require 'time'
+
 Facter.add(:profiles) do
   confine :kernel => 'Darwin'
   setcode do
-	profiles = []
+	profiles = {}
 
 	if Facter.value(:os)['release']['major'].to_i >= 12
-		output = Facter::Util::Resolution.exec('/usr/bin/profiles -P')
 
-		output.each_line do |line|
-			if line.include? 'profileIdentifier'
-				profiles << line.split(' ')[3]
-			end
+		output = Facter::Util::Resolution.exec('/usr/sbin/system_profiler SPConfigurationProfileDataType -xml')
+
+		for item in Puppet::Util::Plist.parse_plist(output)[0]['_items'][0]['_items']
+			profiles[item['spconfigprofile_profile_identifier']] = {
+				'name' => item['_name'],
+				'description' => item['spconfigprofile_description'],
+				'verification_state' => item['spconfigprofile_verification_state'],
+				'uuid' => item['spconfigprofile_profile_uuid'],
+				'organization' => item['spconfigprofile_organization'],
+				'install_date' => DateTime.parse(item['spconfigprofile_install_date'].scan(/\(([^\)]+)\)/).last.first),
+			}
 		end
 	end
 
