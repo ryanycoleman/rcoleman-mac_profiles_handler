28848deb421caa60dacaa97a220fd3c97972b272
Samuel Keeley <sam.keeley@gmail.com>
2016-08-09 10:22:16 -0500

    Keep receipts to better determine install status
    If profiles are changed outside of Puppet, we previously would not know. With this change, we keep track of the install time of profiles immediately after installation and compare that to what Facter gives us.

--- lib/puppet/provider/profile_manager/osx.rb
+++ lib/puppet/provider/profile_manager/osx.rb
@@ -1,3 +1,5 @@
+require 'puppet/util/plist'
+
 Puppet::Type.type(:profile_manager).provide :osx do
   desc "Provides management of mobileconfig profiles on OS X."
 
@@ -9,6 +11,7 @@ Puppet::Type.type(:profile_manager).provide :osx do
 
   def create
     profilescmd('-I', '-F', resource[:profile])
+    writereceipt()
   end
 
   def destroy
@@ -16,6 +19,61 @@ Puppet::Type.type(:profile_manager).provide :osx do
   end
 
   def exists?
-    return Facter.value(:profiles).include? resource[:name]
+    return installed()
   end
+
+  def installed
+    # if already installed, check if it is the right one.
+    # if not installed, return false.
+    if Facter.value(:profiles).include? resource[:name]
+      return current()
+    else
+      return false
+    end
+  end
+
+  def getinstalleddate
+
+    # must be rerun as the output from Facter's earlier run is now
+    # outdated, but this only runs on refresh so not horrible.
+    output = Puppet::Util::Execution.execute('/usr/sbin/system_profiler SPConfigurationProfileDataType -xml')
+
+    for item in Puppet::Util::Plist.parse_plist(output)[0]['_items'][0]['_items']
+      if item['spconfigprofile_profile_identifier'] == resource[:name]
+        return DateTime.parse(item['spconfigprofile_install_date'].scan(/\(([^\)]+)\)/).last.first)
+      end
+    end
+
+  end
+
+  def getreceipts
+    begin
+      receipts = Puppet::Util::Plist.read_plist_file(Facter.value(:puppet_vardir) + '/mobileconfigs/receipts.plist')
+    rescue IOError, Errno::ENOENT
+      receipts = {}
+    end
+
+    return receipts
+  end
+
+  def writereceipt
+    # get install time from profile, write to disk so we know if the
+    # currently installed profile is the one we installed, this uses
+    # code from the fact but needs to re-run immediately.
+    receipts = getreceipts()
+
+    receipts[resource[:name]] = {'install_date'=> getinstalleddate()}
+
+    Puppet::Util::Plist.write_plist_file(receipts, Facter.value(:puppet_vardir) + '/mobileconfigs/receipts.plist')
+  end
+
+  def current
+    begin
+      return getinstalleddate().to_time == getreceipts()[resource[:name]]['install_date']
+    rescue NoMethodError
+      # no matching receipt
+      return false
+    end
+  end
+
 end
