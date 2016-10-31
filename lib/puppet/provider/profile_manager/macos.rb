bdb1e31aa751a0b7f8fc3723d3d56b9b7518547a
Samuel Keeley <sam.keeley@gmail.com>
2016-10-31 12:36:15 -0500

    rework, now gets state during run as it may change from other sources

--- lib/puppet/provider/profile_manager/macos.rb
+++ lib/puppet/provider/profile_manager/macos.rb
@@ -22,36 +22,29 @@ Puppet::Type.type(:profile_manager).provide :macos do
     # if already installed, check if it is the right one.
     # if not installed, return false.
     # if we are removing, don't care if it is the right one.
-    if Facter.value(:profiles).include? resource[:name]
+    state = getinstalledstate
+    if state != false
       if resource[:ensure] == :absent
         return true
       else
-        return current
+        begin
+          return state['install_date'].to_time == getreceipts[resource[:name]]['install_date']
+        rescue NoMethodError
+          # no matching receipt
+          return false
+        end
       end
     else
       return false
     end
   end
 
-  def getinstalleddate
-    # must be rerun as the output from Facter's earlier run is now
-    # outdated, but this only runs on refresh so not horrible.
-    output = Puppet::Util::Execution.execute('/usr/sbin/system_profiler SPConfigurationProfileDataType -xml')
-
-    for item in Puppet::Util::Plist.parse_plist(output)[0]['_items'][0]['_items']
-      if item['spconfigprofile_profile_identifier'] == resource[:name]
-        return DateTime.parse(item['spconfigprofile_install_date'].scan(/\(([^\)]+)\)/).last.first)
-      end
-    end
-  end
-
   def getreceipts
     begin
-      receipts = Puppet::Util::Plist.read_plist_file(Facter.value(:puppet_vardir) + '/mobileconfigs/receipts.plist')
+      receipts = Puppet::Util::Plist.read_plist_file(Puppet[:vardir] + '/mobileconfigs/receipts.plist')
     rescue IOError, Errno::ENOENT
       receipts = {}
     end
-
     receipts
   end
 
@@ -61,15 +54,28 @@ Puppet::Type.type(:profile_manager).provide :macos do
     # code from the fact but needs to re-run immediately.
     receipts = getreceipts
 
-    receipts[resource[:name]] = { 'install_date' => getinstalleddate }
+    receipts[resource[:name]] = { 'install_date' => getinstalledstate['install_date'] }
 
-    Puppet::Util::Plist.write_plist_file(receipts, Facter.value(:puppet_vardir) + '/mobileconfigs/receipts.plist')
+    Puppet::Util::Plist.write_plist_file(receipts, Puppet[:vardir] + '/mobileconfigs/receipts.plist')
   end
 
-  def current
-    return getinstalleddate.to_time == getreceipts[resource[:name]]['install_date']
-  rescue NoMethodError
-    # no matching receipt
+  def getinstalledstate
+    output = Puppet::Util::Execution.execute('/usr/sbin/system_profiler SPConfigurationProfileDataType -xml')
+
+    data = Puppet::Util::Plist.parse_plist(output)[0]['_items']
+
+    unless data.empty?
+      for item in data[0]['_items']
+        if item['spconfigprofile_profile_identifier'] == resource[:name]
+          return {
+            'identifier' => item['spconfigprofile_profile_identifier'],
+            'display_name' => item['_name'],
+            'uuid' => item['spconfigprofile_profile_uuid'],
+            'install_date' => DateTime.parse(item['spconfigprofile_install_date'].scan(/\(([^\)]+)\)/).last.first)
+          }
+        end
+      end
+    end
     return false
   end
 end
